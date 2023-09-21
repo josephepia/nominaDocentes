a22f94bdf5a41bfedfbe09e0b9de177a841a8e40
Joseph Epiayu Fernandez <remixlebo@gmail.com>
2023-09-21 11:43:24 -0500

    feat: changes for user application view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    def resource
+        @resource ||= User.new
+    end 
+
+    def resource_name 
+        :user
+    end
+
+    def resource_class
+        User
+    end
+
+    def user
+        @user ||= User.new
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -38,4 +38,8 @@ class User < ApplicationRecord
   has_and_belongs_to_many :roles
 
 
+
+  def is_teacher
+    false
+  end 
 end
