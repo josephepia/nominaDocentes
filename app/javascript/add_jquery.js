09636f1d1861b3fa8b1f1f56522dbd397ae1cd2d
Joseph Epiayu Fernandez <remixlebo@gmail.com>
2023-09-21 18:11:33 -0500

    fix: fix sidenav between pages

--- app/javascript/add_jquery.js
+++ app/javascript/add_jquery.js
@@ -2,14 +2,35 @@
 // // window.jQuery = jquery;
 // // window.$ = jquery;
 
-window.jQuery = jquery;
-window.$ = jquery;
-window.jquery = jquery;
+// window.jQuery = jquery;
+// window.$ = jquery;
+// window.jquery = jquery;
+
+// $(document).on('turbolinks:before-visit turbolinks:before-cache', () => {
+//   M.AutoInit();
+// });
+
+// $(document).on('turbolinks:load', () => {
+//   M.AutoInit();
+//   elem = document.querySelector('#slide-out');
+//   instance = new M.Sidenav(elem, {});
+//   $(".dropdown-trigger").dropdown({});
+
+// });
+// $(document).on('turbolinks:before-cache', () => {
+//   M.AutoInit();
+//   const elem = document.querySelector('#slide-out');
+//   const instance = M.Sidenav.getInstance(elem);
+//   if (instance) {
+//     instance.destroy();
+//   }
+// });
 
 // $(document).ready(function(){
 //     $('.sidenav').sidenav();
 //   });
-$(function(){
-    $('.sidenav').sidenav();
-  });
+// M.AutoInit();
+// $(function () {
+//   $('.sidenav').sidenav();
+// });
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,29 @@ import "controllers"
 import "jquery"
 import "materialize";
 
-$(function(){
-    $('.sidenav').sidenav();
-  });
+document.addEventListener('turbo:load', () => {
+  M.AutoInit();
+  var elem = document.querySelector('#slide-out');
+  var instance = new M.Sidenav(elem, {});
+});
+document.addEventListener('turbo:before-visit', () => {
+  M.AutoInit();
+  var elem = document.querySelector('#slide-out');
+  var instance = M.Sidenav.getInstance(elem);
+  if (instance) {
+    instance.destroy();
+  }
+});
+
+// document.addEventListener('turbo:before-cache', () => {
+//   console.log("before cache")
+// });
+// document.addEventListener('turbo:before-render', () => {
+//   console.log("before render")
+// });
+// document.addEventListener('turbo:before-visit', () => {
+//   console.log("before visit")
+// });
+// document.addEventListener('turbo:load', () => {
+//   console.log("before load")
+// });
